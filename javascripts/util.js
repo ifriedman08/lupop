ec2bf9d51a2034e22c09964219468b842f3edd59
ifriedman08 <i.friedman.08@gmail.com>
2017-03-10 23:05:25 -0800

    init commit

---
+++ javascripts/util.js
@@ -0,0 +1,58 @@
+$("div.main-nav-link > div")
+.mousedown(function (el) {
+  console.log('mousedown');
+  $(this).css({
+    'background-color' : 'black',
+    'color'  : 'white'
+  })
+})
+.mouseup(function () {
+  console.log('mouseup');
+  $(this).css({
+    'background-color' : 'white',
+    'color'  : 'black'
+  })
+});
+
+var clearBody = function () {
+  $("div#home").hide();
+  $("div#artist").hide();
+  $("div#hair").hide();
+  $("div#contact").hide();
+}
+
+$("div.hair").mouseup(function (el) {
+  clearBody();
+  $('div#hair').show();
+  $('body').css({
+    "background-color":"black",
+    "color":"white"
+  })
+});
+
+$("div.art").mouseup(function (el) {
+  clearBody();
+  $('div#artist').show();
+  $('body').css({
+    "background-color":"black",
+    "color":"white"
+  })
+});
+
+$("div.contact").mouseup(function (el) {
+  clearBody();
+  $('div#contact').show();
+  $('body').css({
+    "background-color":"black",
+    "color":"white"
+  })
+});
+
+$("a#back-to-main").click(function () {
+  clearBody();
+  $('div#home').show();
+  $('body').css({
+    "background-color":"white",
+    "color":"black"
+  })
+})
